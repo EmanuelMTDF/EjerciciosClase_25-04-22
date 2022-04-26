e688e9ec7047e6099ccf0b7e49e04ac0cbf61ff7
EmanuelMTDF <104039755+EmanuelMTDF@users.noreply.github.com>
2022-04-26 19:41:57 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,65 @@
+//PRÁCTICA!!
+//CALCULADORA DE OPERACIONES BASICAS
+
+let numero_1 : number;
+let numero_2 : number;
+
+numero_1 = 30;
+numero_2 = 15;
+
+let suma = numero_1 + numero_2;
+let resta = numero_1 - numero_2;
+let multiplicacion = numero_1 * numero_2;
+let division = numero_1 / numero_2;
+
+console.log (suma);
+console.log (resta);
+console.log (multiplicacion);
+console.log (division);
+
+console.clear();
+
+// EJERCICIOS CLASE 25/04/22 CON CECILIA
+// EJERCICIO CALCULO DE DESCUENTO
+
+let precioInicial : number;
+let descuento : number;
+let precioFinal : number;
+
+precioInicial = 450.50
+descuento = precioInicial * 0.1
+precioFinal = precioInicial - descuento
+
+console.log ("El precio final es:" precioFinal);
+
+// EJERCICIO "AUTOS DE CARRERA"
+
+let tiempoFirstLap : number;
+let tiempoSecondLap : number;
+let tiempoThirdLap : number;
+let tiempoFourthLap : number;
+
+tiempoFirstLap = 10
+tiempoSecondLap = 20
+tiempoThirdLap = 5
+tiempoFourthLap = 5
+
+let tiempoTotal : number = tiempoFirstLap + tiempoSecondLap + tiempoThirdLap + tiempoFourthLap
+let tiempoPromedio : number = tiempoTotal / 4
+
+console.log ("El tiempo total es de:"tiempoTotal "segundos");
+console.log ("El tiempo promedio es de:"tiempoPromedio "segundos");
+
+// FIN DE CLASE 25/04/22
+
+
+
+
+
+
+
+
+
+
+
+
